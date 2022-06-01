8458f1835376cb66ecb8cbddd48e54ef34b5b5c6
David Gustavsson <david.e.gustavsson@gmail.com>
2022-06-01 11:41:38 +0200

    Add simple test, change dependencies

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,17 @@
 using Circuits
+using Unitful
 using JuliaFormatter
 using Test
 
 @testset "Circuits.jl" begin
-    # No tests yet... TODO
+    r = Resistor(5.0u"Ω")
+    c = Capacitor(2.0u"μF")
+    l = Inductor(1.0u"mH")
+    s = Short()
+    o = Open()
+    lcr = Series(l, c, r)
+
+    @test Circuits.impedance(lcr, 5u"kHz") ≈ 110.0u"Ω"
 end
 
 @testset "Formatting" begin
